113192714510b4fb5d72e182132b70539743dc47
nozim8103-crypto <nozim8103@gmail.com>
2026-06-11 08:20:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from yt_dlp import YoutubeDL
 
 # Sozlamalar
 TOKEN = "8870187278:AAGqNxIYK1sKADwFzSUlbOsUoeP_s_XuPtw"
-CHANNEL = "@insta_mobil"
+CHANNEL = "@temuzikinsta
 SECOND_BOT_URL = "https://t.me/insta_hesh_bot"
 
 def extract_hashtags(text):
